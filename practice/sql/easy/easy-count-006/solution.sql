41e46a02d0078e56f4396e463d6d506507f26c1d
Ghun06 <95094186+Ghun06@users.noreply.github.com>
2026-06-09 02:42:20 +0000

    solve(sql/easy): easy-count-006

---
+++ practice/sql/easy/easy-count-006/solution.sql
@@ -0,0 +1,6 @@
+-- Xom Data · Total number of bank accounts
+-- Problem: https://xomdata.com/practice/easy-count-006
+-- Solved: 2026-06-09
+
+-- Viết SQL của bạn ở đây
+SELECT COUNT(*) AS total_accounts FROM accounts
